9826f481ef427030b91d23b542773c97cfcb8ca6
stifler33 <bagi33.ru@gmail.com>
2021-05-20 23:09:04 +0300

    task 1 checkout

--- task1.cpp
+++ task1.cpp
@@ -2,7 +2,26 @@
 #include <vector>
 using namespace std;
 int main() {
-    vector<int> array = {10,-15,22,5,44,-56,-2,52,4,-6};
-    for (int i : array);
+    vector<int> array = {-2,1,-3,4,-1,2,1,-5,4};
+
+    int maxI = 0;
+    int minI = 0;
+    int maxSum = 0;
+    int sum = 0;
+    for (int i = 0; i < array.size(); i++){
+        sum += array[i];
+        if (sum > maxSum){
+            maxSum = sum;
+            maxI = i;
+        }
+    }
+    for (int i = maxI; i >= 0; i--){
+        maxSum -= array[i];
+        if (maxSum == 0) {
+            minI = i;
+        }
+    }
+    cout << "max index " << maxI << endl;
+    cout << "min index " << minI << endl;
     return 0;
 }
