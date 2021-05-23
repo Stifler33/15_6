7cf52639b779234620b9a6b47c10e7df4ec7c629
stifler33 <bagi33.ru@gmail.com>
2021-05-23 14:55:20 +0300

    task 3 work

--- task2.cpp
+++ task2.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 int main() {
-    vector<int> vec = {2,15,14,7};
+    vector<int> vec = {2,3,7,9,4,6,1};
     int result = 9;
     for (int i = 0; i < vec.size(); i++){
         int sum = vec[i];
--- task3.cpp
+++ task3.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
-
+#include <vector>
+using namespace std;
+vector<int> sort_Vec(vector<int> &vec){
+    for (int i = 0; i < vec.size(); i++){
+        for (int j = i+1; j < vec.size(); j++){
+            if (vec[j] < vec[i]){
+                int buf = vec[i];
+                vec[i] = vec[j];
+                vec[j] = buf;
+            }
+        }
+    }
+    return vec;
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<int> vec;
+    int input = 0;
+    while (input != -2){
+        cin >> input;
+        if (input == -1){
+            sort_Vec(vec);
+            cout << vec[4] << endl;
+            for (int i : vec){
+                cout << i << ' ';
+            }
+            cout << endl;
+        }else if (input == -2){
+            return 0;
+        }else{
+            vec.push_back(input);
+        }
+
+    }
     return 0;
 }
