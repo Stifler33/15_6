2ad7ca638aef8eb20f7dd41a32927a11278555fa
stifler33 <bagi33.ru@gmail.com>
2021-05-23 14:04:44 +0300

    task 2 work

--- task1.cpp
+++ task1.cpp
@@ -2,34 +2,29 @@
 #include <vector>
 using namespace std;
 int main() {
-    vector<int> array = {2,-1,2,5,0,-5,-1,2,4};
-
+    vector<int> array = {5,3,2,5,0,5,1,2,4};
     int maxI = 0;
     int minI = 0;
-    int maxSum = 0;
-    int sum = 0;
-    for (int i = 0; i < array.size(); i++){
-        sum += array[i];
-        if (sum > maxSum){
-            maxSum = sum;
+    for (int i = 0, counter = 0,maxSum = 0;  i < array.size(); i++){
+        counter += array[i];
+        if (counter > maxSum){
+            maxSum = counter;
             maxI = i;
         }
     }
-    int minSum = 0;
-
+    int sum = 0;
     for (int j = 0; j < maxI; j++){
-        int sum2 = 0;
+        int counter = 0;
         for (int g = j ; g <= maxI; g++){
-            sum2 += array[g];
+            counter += array[g];
         }
-        if (sum2 > minSum) {
-            minSum = sum2;
+        if (counter > sum) {
+            sum = counter;
             minI = j;
         }
     }
-
     cout << "max index " << maxI << endl;
     cout << "min index " << minI << endl;
-    cout << "min Sum " << minSum << endl;
+    cout << "min Sum " << sum << endl;
     return 0;
 }
--- task2.cpp
+++ task2.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
-
+#include <vector>
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    vector<int> vec = {2,15,14,7};
+    int result = 9;
+    for (int i = 0; i < vec.size(); i++){
+        int sum = vec[i];
+        for (int j = i+1; j < vec.size(); j++){
+            if (sum + vec[j] == result){
+                cout << sum << endl;
+                cout << vec[j] << endl;
+                return 0;
+            }
+        }
+    }
 }
