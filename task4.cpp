b3a0a7521136951bd278e722e8125bc1685bfd23
stifler33 <bagi33.ru@gmail.com>
2021-05-26 22:26:06 +0300

    task 4 modified

--- task4.cpp
+++ task4.cpp
@@ -2,23 +2,20 @@
 #include <cmath>
 #include <vector>
 using namespace std;
-vector<int> sort_Vec(vector<int> &vec){
-    for (int i = 0; i < vec.size(); i++){
-        for (int j = i+1; j < vec.size(); j++){
-            if (abs(vec[j]) < abs(vec[i])){
-                int buf = vec[i];
-                vec[i] = vec[j];
-                vec[j] = buf;
-            }
-        }
-    }
-    return vec;
-}
 int main() {
-    vector<int> vec = {1,-5,7,-8,-4,6,-7,-2,5,15,6};
-    sort_Vec(vec);
-    for (int i : vec){
-        cout << i << " ";
+    vector<int> vec = {-8,-7,-5,-4,-2,1,3,6,15};
+    int start;
+    for (int s = 0; s < vec.size() && vec[s] < 0; s++){
+        start = s + 1;
+    }
+    for (int i = start - 1; i >= 0;){
+        if (vec[start] > abs(vec[i])){
+            cout << vec[i] << " ";
+            i--;
+        }else {
+            cout << vec[start] << " ";
+            start++;
+        }
     }
     return 0;
 }
